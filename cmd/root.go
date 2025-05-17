958d380215904194389d9efbf350a4802fc61e0e
Kasper Therkelsen <kath@aura.dk>
2025-05-17 18:57:13 +0200

    I DID IT, I MADE IT PRETTY

--- cmd/root.go
+++ cmd/root.go
@@ -3,10 +3,11 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"io"
 	"log"
 	"net/http"
 	"os"
+	"io"
+	"encoding/json"
 )
 
 var rootCmd = &cobra.Command{
@@ -15,12 +16,24 @@ var rootCmd = &cobra.Command{
 	Long:  "lunch-cli is a cli tool that fetches the menu for the location in Skanderborgvej 190",
 	Run: func(cmd *cobra.Command, args []string) {
 		response, err := http.Get("https://shop.foodandco.dk/api/WeeklyMenu?restaurantId=1234&languageCode=da-DK")
-
-		responseData, err := io.ReadAll(response.Body)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(string(responseData))
+		defer response.Body.Close()
+
+		body, err := io.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var jsonData interface{}
+		if err := json.Unmarshal(body, &jsonData); err != nil {
+			panic(err)
+		}
+		prettyJson, err := json.MarshalIndent(jsonData, ""," ")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(prettyJson))
 	},
 }
 
